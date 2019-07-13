27367475415d836781af89a4e3b1c0edf005e6c9
Hiromichi Suzuki <jakephot@gmail.com>
2019-07-14 00:15:41 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ea Ninja Client Sample (PHP)
+
+## Dev
+
+```
+$ php -S localhost:8080
+```
---
+++ config.sample.php
@@ -0,0 +1,5 @@
+<?php
+define('BASE_URL', "https://dev.eaninja.eu/api/v1");
+define('PRODUCT_ID', "");
+define('EANINJA_API_KEY', "");
+define('EANINJA_API_SECRET', "");
